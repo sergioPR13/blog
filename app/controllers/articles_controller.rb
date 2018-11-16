f2bb60d0ee951b51ffefa2ee9815627c342e2247
sergio <srgpr13@gmail.com>
2018-11-16 08:35:50 -0800

    segundo commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+    #GET /articles
+    def index
+        @articles = Article.all
+    end
+    
+    #GET /articles/:id
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    #GET /articles/new
+    def new
+        @article = Article.new
+    end
+    
+    #POST /articles
+    def create
+        @article=Article.new(title: params[:article][:title], body: params[:article][:body])
+        @article.save
+        redirect_to @article
+    end
+
+    # PUT /article/:id
+    def update
+
+    end
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+  resources :articles
+=begin
+  get "/articles" index
+  post "/articles" create
+  delete "/articles" delete
+  get "/articles/:id" show
+  get "/articles/new" new
+  get "/articles/:id/edit" edit
+  patch "/articles/:id" update
+  put "/articles/:id" update
+=end
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
